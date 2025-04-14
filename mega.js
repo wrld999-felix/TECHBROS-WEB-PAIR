d2f161ebfc32c8af6525d95d1778188d16c314bd
wrld999-felix <immanuelfelix999@gmail.com>
2025-04-14 20:13:17 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'emmanuelfelix829@gmail.com',
-    password: 'gamercoder999@',
+    email: 'hiltonvidz15@gmail.com',
+    password: '@H-vidz15',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
